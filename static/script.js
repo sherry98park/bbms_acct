0ac159fc842992a2e99e5d8b7eed814e517b2d84
sherry98park <sc.sherry98.park@gmail.com>
2025-05-31 13:50:10 +0900

    전학생 1명 더 추가

--- static/script.js
+++ static/script.js
@@ -4,6 +4,7 @@ const accounts = [
   { id: "2023002", name: "김유진", userId: "biko02", password: "pw5678" },
   { id: "2023003", name: "이민수", userId: "biko03", password: "biko7890" }
   { id: "2023004", name: "꽃님이", userId: "biko04", password: "biko7891" }
+  { id: "2023005", name: "꽃순이", userId: "biko05", password: "biko7893" }
 ];
 
 // 폼 제출 이벤트 처리
